ed7f79c7564eec8e5aa2e8195ba5141928b36ad7
peppelinux <giuseppe.demarco@unical.it>
2021-02-12 16:54:33 +0100

    settingslocal is something different, but ok

--- djangosaml2_sp/djangosaml2_sp/settings.py
+++ djangosaml2_sp/djangosaml2_sp/settings.py
@@ -12,38 +12,10 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 
+from . settingslocal import *
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'btl-x0ja09$zqer3h^n^_ic!9h+1q0g!-wqzj&&zio@(@5p*no'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['*']
-
-# Application definition
-
-INSTALLED_APPS = [
-    # custom user model
-    'custom_accounts',
-
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-
-    # SAML2 SP
-    'djangosaml2',
-    'saml2_sp',
-    'djangosaml2_spid'
-]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -110,7 +82,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-AUTH_USER_MODEL = 'custom_accounts.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
@@ -150,39 +121,6 @@ if 'saml2_sp' in INSTALLED_APPS or \
 
 # SPID SP
 if 'djangosaml2_spid' in INSTALLED_APPS:
-    from settingslocal import *
+    from . spid_settingslocal import *
 elif 'saml2_sp' in INSTALLED_APPS:
     from . sp_pysaml2_satosa import *
-
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        },
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-        'djangosaml2': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-        },
-    }
-}
